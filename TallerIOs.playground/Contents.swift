dc26ca6415150fec6dc91c7ddfbb30712394270e
UrielMorenoMartinez <uriel.tec3234@gmail.com>
2018-10-31 11:56:34 -0700

    CalculadoraInicio - sin terminar

---
+++ TallerIOs.playground/Contents.swift
@@ -0,0 +1,74 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*print("Hello world")
+
+var diccionario = ["😀":"Feliz"]
+
+diccionario["😀"]
+
+
+var x1 = 18
+var x23 = 42
+var xdeVerdad = 444
+var xFinal = 26
+
+var suma = x1 + x23 + xdeVerdad + xFinal
+int
+suma = suma + 10000000
+
+var numeroGrande = 1000_000_000
+
+
+var a = 15
+var b = 10
+
+var suma2 = a + b
+ 
+ 
+
+//clase 2
+
+var variableUno = 10
+
+variableUno = 11
+print(variableUno)
+
+let constanteUno = 10
+
+print(constanteUno)
+
+
+var dato = "hola"
+var datoNumerico = 10
+
+var datoFlotante = 10.5
+
+Int64.max
+
+var numeroOchoBits:Int8=64
+
+var numeroFloat:Float=1123234234234234234
+var numero:Double = 1123234234234234234
+
+var caracter:Character = "u"
+
+var cadenaTexto:String = "Uriel"
+
+//clase 3*/
+
+var edad = 17
+
+if edad > 18 {
+    print("Soy Mayor de edad")
+}else{
+    print("Soy Menor de edad")
+}
+
+switch edad {
+case 18:
+    print("Soy Mayor de edad")
+default:
+   print("Soy Menor de edad")
+}
